28ea34a17008029adf7cfa7167039e37f10a3dcf
Sam Atkins <git@samatkins.co.uk>
2017-07-29 12:35:28 +0100

    Showing some stuff in a rudimentary UI

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using System.Collections;
+﻿
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
-[Serializable]
+[System.Serializable]
 public struct Consequence
 {
 	public string field;
 	public float minChange, maxChange;
 }
 
-[Serializable]
+[System.Serializable]
 public struct EventChoice
 {
 	public string description;
 	public Consequence[] consequences;
 }
 
-[Serializable]
+[System.Serializable]
 public struct Event
 {
 	public string character;
@@ -26,7 +26,7 @@ public struct Event
 	public EventChoice[] choices;
 }
 
-[Serializable]
+[System.Serializable]
 public struct EventArray
 {
 	public Event[] events;
@@ -35,8 +35,15 @@ public struct EventArray
 public class Controller : MonoBehaviour {
 	public List<Event> events = new List<Event>();
 
+	// UI stuff
+	public Text characterText;
+	public Text eventText;
+	public Button choiceButtonPrefab;
+	public GameObject choiceButtonsGroup;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		var eventGuids = AssetDatabase.FindAssets("t:TextAsset", new string[] { "Assets/events" });
 		foreach (var guid in eventGuids)
 		{
@@ -44,10 +51,32 @@ public class Controller : MonoBehaviour {
 			var loadedEvents = JsonUtility.FromJson<EventArray>(eventAsset.text);
 			events.AddRange(loadedEvents.events);
 		}
+
+		ShowRandomEvent();
+	}
+
+	public void ShowRandomEvent()
+	{
+		var e = events[Random.Range(0, events.Count)];
+		characterText.text = e.character;
+		eventText.text = e.description;
+
+		foreach (Transform oldButton in choiceButtonsGroup.transform)
+		{
+			GameObject.Destroy(oldButton.gameObject);
+		}
+		
+		foreach (var choice in e.choices)
+		{
+			var button = Instantiate(choiceButtonPrefab, choiceButtonsGroup.transform) as Button;
+			button.GetComponentInChildren<UnityEngine.UI.Text>().text = choice.description;
+		}
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 }
