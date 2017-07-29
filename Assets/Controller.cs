6cdf4888a282f5865a7c9081cd0da4c7872a9576
Sam Atkins <git@samatkins.co.uk>
2017-07-29 15:03:59 +0100

    Court view! It's awful and hacky like everything else in the UI. I hate Unity's UI stuff so much. :'(

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -32,11 +32,50 @@ public struct EventArray
 	public Event[] events;
 }
 
+public struct Status
+{
+	public int day;
+	public float money;
+	public float love;
+	public float respect;
+
+	Status(float initialMoney = 100)
+	{
+		day = 0;
+		love = 0;
+		respect = 0;
+		money = initialMoney;
+	}
+
+	public override string ToString()
+	{
+		return string.Format("Status {{ Day: {3}, Money: {0}, Love: {1}, Respect: {2} }}", money, love, respect, day);
+	}
+}
+
 public class Controller : MonoBehaviour {
-	public List<Event> events = new List<Event>();
+
+	public Status status = new Status();
+
+	public List<Event> allEvents = new List<Event>();
+
+	public List<Event> unseenEvents = new List<Event>();
+	public List<Event> seenEvents = new List<Event>();
+	public List<Event> courtEvents = new List<Event>(); // events currently available as options
+
 	public Event currentEvent;
 
-	// UI stuff
+	// Configuration
+	public int eventsToShowPerDay = 5;
+
+	// Court UI
+	public GameObject courtPanel;
+	public Text courtDescriptionText;
+	public PetitionButton petitionButtonPrefab;
+	public GameObject petitionButtonsGroup;
+
+	// Event UI
+	public GameObject eventPanel;
 	public Text characterText;
 	public Text eventText;
 	public ChoiceButtonScript choiceButtonPrefab;
@@ -50,18 +89,85 @@ public class Controller : MonoBehaviour {
 		{
 			TextAsset eventAsset = AssetDatabase.LoadAssetAtPath<TextAsset>(AssetDatabase.GUIDToAssetPath(guid));
 			var loadedEvents = JsonUtility.FromJson<EventArray>(eventAsset.text);
-			events.AddRange(loadedEvents.events);
+			allEvents.AddRange(loadedEvents.events);
 		}
 
-		ShowRandomEvent();
+		BeginGame();
 	}
 
-	public void ShowRandomEvent()
+	public void BeginGame()
 	{
-		// TODO: We probably want some kind of random bucket system: at first we put all events in the bucket,
-		// then we take a random one out each time. This eliminates repeats until we've seen every event once.
-		// (Then we just swap the used/unused buckets.)
-		currentEvent = events[Random.Range(0, events.Count)];
+		courtPanel.SetActive(false);
+		eventPanel.SetActive(false);
+
+		unseenEvents.Clear();
+		seenEvents.Clear();
+		courtEvents.Clear();
+
+		unseenEvents.AddRange(allEvents);
+
+		StartDay();
+	}
+
+	public void StartDay()
+	{
+		status.day++;
+
+		// ensure there are X events in the court today
+		while (courtEvents.Count < eventsToShowPerDay)
+		{
+			if (unseenEvents.Count > 0)
+			{
+				//move random item to court
+				Event e = unseenEvents[Random.Range(0, unseenEvents.Count)];
+				unseenEvents.Remove(e);
+				courtEvents.Add(e);
+			}
+			else if (seenEvents.Count > 0)
+			{
+				// move seen events back to unseen
+				unseenEvents.AddRange(seenEvents);
+				seenEvents.Clear();
+			}
+			else
+			{
+				// We're out of events! We have to stop looking.
+				break;
+			}
+		}
+
+		ShowCourt();
+	}
+
+	public void ShowCourt()
+	{
+		// Now, init the court display
+
+		courtDescriptionText.text = string.Format("There are {0} people waiting in your court.", courtEvents.Count);
+
+		foreach (Transform oldButton in petitionButtonsGroup.transform)
+		{
+			GameObject.Destroy(oldButton.gameObject);
+		}
+
+		foreach (var e in courtEvents)
+		{
+			var button = Instantiate(petitionButtonPrefab, petitionButtonsGroup.transform) as PetitionButton;
+			button.Init(this, e);
+		}
+
+		courtPanel.SetActive(true);
+		eventPanel.SetActive(false);
+	}
+
+	public void OnEventSelected(Event e)
+	{
+		ShowEvent(e);
+	}
+
+	public void ShowEvent(Event e)
+	{
+		currentEvent = e;
 		characterText.text = currentEvent.character;
 		eventText.text = currentEvent.description;
 
@@ -76,17 +182,53 @@ public class Controller : MonoBehaviour {
 			var button = Instantiate(choiceButtonPrefab, choiceButtonsGroup.transform) as ChoiceButtonScript;
 			button.Init(this, choice, choiceIndex);
 		}
+
+		courtPanel.SetActive(false);
+		eventPanel.SetActive(true);
+	}
+
+	public void HideEvent()
+	{
+		ShowCourt();
+	}
+
+	float Adjust(float startValue, Consequence consequence)
+	{
+		float result = startValue;
+
+		float adjustment = Random.Range(consequence.minChange, consequence.maxChange);
+
+		result += adjustment;
+
+		return result;
 	}
 
 	public void OnChoiceSelected(int choiceIndex)
 	{
 		Debug.Log("Selected choice " + choiceIndex + "!");
-		ShowRandomEvent();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-	{
-		
+
+		courtEvents.Remove(currentEvent);
+
+		var choice = currentEvent.choices[choiceIndex];
+
+		foreach (var consequence in choice.consequences)
+		{
+			switch (consequence.field)
+			{
+				case "money": status.money = Adjust(status.money, consequence); break;
+				case "love": status.love = Adjust(status.love, consequence); break;
+				case "respect": status.respect = Adjust(status.respect, consequence); break;
+				default: break;
+			}
+		}
+
+		seenEvents.Add(currentEvent);
+
+		// TODO: Update UI!
+		Debug.Log(status);
+
+		// TODO: check if we lost!
+
+		ShowCourt();
 	}
 }
---
+++ Assets/PetitionButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PetitionButton : MonoBehaviour
+{
+	public Controller controller;
+	public Event e;
+
+	public void Init(Controller controller, Event e)
+	{
+		this.controller = controller;
+		this.e = e;
+
+		GetComponentInChildren<Button>().onClick.AddListener(OnClicked);
+		GetComponentInChildren<Text>().text = e.character;
+	}
+
+	void OnClicked()
+	{
+		controller.OnEventSelected(e);
+	}
+}
